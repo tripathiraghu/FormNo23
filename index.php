0effb452e3f21e8a746bd940e9de24897314b56f
Tripathi Raghu <48356533+tripathiraghu@users.noreply.github.com>
2020-10-01 00:23:15 +0530

    Changes

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>eForm - By One Shop Seva</title>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>eForm - By One Shop Seva</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
 	<link rel="icon" type="image/png" href="images/favicon.ico"/>
 <!--===============================================================================================-->
